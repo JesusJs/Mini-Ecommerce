c5e9db2d16945ee7ecddb863762a24fdc4247595
Jesus Chavez <jdchavezzss@gmail.com>
2021-02-06 12:55:10 -0400

    Primera Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './shared/inicio/inicio.component';
+import { NosotrosComponent } from './shared/nosotros/nosotros.component';
+import { ContactoComponent } from './shared/contacto/contacto.component';
+import { PagosComponent } from './shared/pagos/pagos.component';
+import { CardsComponent } from './shared/cards/cards.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'inicio',   component: InicioComponent   },
+  { path: 'pagos',    component: PagosComponent    },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/inicio'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{ FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { InicioComponent } from './shared/inicio/inicio.component';
+import { NosotrosComponent } from './shared/nosotros/nosotros.component';
+import { ContactoComponent } from './shared/contacto/contacto.component';
+import { PagosComponent } from './shared/pagos/pagos.component';
+import { CardsComponent } from './shared/cards/cards.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    PagosComponent,
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
